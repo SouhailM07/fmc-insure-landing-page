bb8da2dd68e034c5b87751f1bea5b7f17d17b1a3
shadow <lightshadow416@gmail.com>
2024-01-23 19:31:11 +0100

    adding colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,15 @@
 /** @type {import(tailwindcss).Config} */ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        DarkViolet: "hsl(256, 26%, 20%)",
+        GrayishBlue: "hsl(216, 30%, 68%)",
+        VeryDarkViolet: "hsl(270, 9%, 17%)",
+        DarkGrayishViolet: "hsl(273, 4%, 51%)",
+        VeryLightGray: "hsl(0, 0%, 98%)",
+      },
+    },
+  },
   plugins: [],
 };
